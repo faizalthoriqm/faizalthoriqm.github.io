d74e3cfefecadd50bf10cbaf0110daf962889014
faizalthoriqm <50917728+faizalthoriqm@users.noreply.github.com>
2025-10-09 11:54:46 +0700

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,7 +134,36 @@ for (let i = 0; i < formInputs.length; i++) {
   });
 }
 
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.LOGO');
 
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    setTimeout(()=>{
+
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx + 1) * 400) 
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50)
+
+            })
+        },2000);
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        },2300);
+    })
+})
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
